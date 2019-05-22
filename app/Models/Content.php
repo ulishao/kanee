c683aeba1f210ed2621acec12d80902e332a92dd
lishaowei <980167048@qq.com>
2019-05-22 20:40:59 +0800

    提交
    Signed-off-by: shaowei <980167048@qq.com>

---
+++ app/Models/Content.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ * Date: Wed, 22 May 2019 12:39:31 +0000.
+ */
+
+namespace App\Models;
+
+use Reliese\Database\Eloquent\Model as Eloquent;
+
+/**
+ * Class Content
+ *
+ * @property string $id
+ * @property string $user_id
+ * @property string $title
+ * @property string $urls
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ *
+ * @package App\Models
+ */
+class Content extends Eloquent
+{
+    public $incrementing = false;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'urls'
+    ];
+}
