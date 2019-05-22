6168ef0e1d9fe3b99110d223f54b3f1ac78ffc39
lishaowei <980167048@qq.com>
2019-05-22 21:13:11 +0800

    提交
    Signed-off-by: shaowei <980167048@qq.com>

--- app/Models/Content.php
+++ app/Models/Content.php
@@ -33,7 +33,7 @@ class Content extends Eloquent
 
     public function user ()
     {
-        return $this->belongsTo (User::class, 'openid', 'user_id');
+        return $this->belongsTo (User::class, 'user_id', 'openid');
     }
 
     public function getUrlsAttribute ( $value )
