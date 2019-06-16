d2766ec9e1c031abb38bc87453535b164309963b
lishaowei <980167048@qq.com>
2019-06-16 11:31:02 +0800

    提交
    Signed-off-by: shaowei <980167048@qq.com>

--- app/Http/Controllers/ImgController.php
+++ app/Http/Controllers/ImgController.php
@@ -40,7 +40,7 @@ class ImgController extends Controller
         }
 //        if(\request ()->get ('category_id'))
 
-        $data = $query->orderBydesc ('title')
+        $data = $query->orderBydesc (['created_at', 'category_id'])
             ->paginate (2);
         return Resource::collection ($data);
     }
