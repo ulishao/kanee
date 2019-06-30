9ecf2b7b026c35e43db84f351b086483298a5063
lishaowei <980167048@qq.com>
2019-06-30 14:53:21 +0800

    提交
    Signed-off-by: shaowei <980167048@qq.com>

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -148,6 +148,7 @@ class UserController extends Controller
 
     public function getIndex ()
     {
+
         return User::select(['latitude', 'longitude', 'sex'])->whereNotNull('latitude')->get();
     }
     public function collect()
